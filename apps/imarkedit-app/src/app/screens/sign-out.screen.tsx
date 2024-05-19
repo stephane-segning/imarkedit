f90b61b5fb7f694c853237f041e0e1e3433fffc8
Stephane SEGNING LAMBOU <selastlambou@gmail.com>
2024-05-19 20:33:23 +0200

    chore: backup

--- apps/imarkedit-app/src/app/screens/sign-out.screen.tsx
+++ apps/imarkedit-app/src/app/screens/sign-out.screen.tsx
@@ -1,19 +1,22 @@
 import { useAuth } from '../services/auth';
 import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { LoadingScreen } from './loading.screen';
+import { useError } from 'react-use';
 
 export function SignOutScreen() {
   const navigate = useNavigate();
+  const dispatch = useError();
   const { logout } = useAuth();
-  const signOut = async () => {
+  const signOut = useCallback(async (): Promise<void> => {
     try {
       await logout();
       navigate('/');
     } catch (error) {
       console.error(error);
+      dispatch(error as Error);
     }
-  };
+  }, [dispatch, logout, navigate]);
 
   useEffect(() => {
     signOut();
